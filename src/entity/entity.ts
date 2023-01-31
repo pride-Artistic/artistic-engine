f5d2def7ded79089e9063514369d84d4349af6ec
friend1226 <friendcha7749@gmail.com>
2023-01-31 23:42:35 +0900

    Add "dimension" field / Updated gitignore file
    - Added "dimension" field as Vector2D, which means (width, height)
    - Added path ".vscode" in ".gitignore" file.

--- src/entity/entity.ts
+++ src/entity/entity.ts
@@ -1,13 +1,39 @@
 import { Vector2D } from "src/vector";
 
 export default class Entity {
+  private dimension: Vector2D = new Vector2D();
+
   private position: Vector2D = new Vector2D();
+
+  public get Dimension() {
+    return this.dimension;
+  }
 
   public get Position() {
     return this.position;
   }
 
+  public get height() {
+    return this.dimension.Y;
+  }
+
+  public get width() {
+    return this.dimension.X;
+  }
+
+  public set Dimension(dimension: Vector2D) {
+    this.dimension = dimension;
+  }
+
   public set Position(pos: Vector2D) {
     this.position = pos;
+  }
+
+  public set height(value: number) {
+    this.dimension.Y = value;
+  }
+
+  public set width(value: number) {
+    this.dimension.X = value;
   }
 }
