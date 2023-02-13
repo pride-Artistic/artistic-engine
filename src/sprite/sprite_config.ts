6f7fa04ff4ce1a397cb1c953a391dbb74fc645a0
Ailre <xtramainpp@gmail.com>
2023-02-13 10:31:57 +0900

    add draw method with self access to drawer

--- src/sprite/sprite_config.ts
+++ src/sprite/sprite_config.ts
@@ -1,8 +1,10 @@
 import EntityConstructorConfig from "../entity/entity_config";
+import Sprite from "./sprite";
 
 export default interface SpriteConstructorConfig
   extends EntityConstructorConfig {
   drawer: (
+    self: Sprite,
     context: CanvasRenderingContext2D,
     delay: number
   ) => void | undefined;
