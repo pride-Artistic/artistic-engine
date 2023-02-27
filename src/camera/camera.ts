7856821f893b2d9bb533f505722a97d5636d4cfa
friend1226 <friendcha7749@gmail.com>
2023-02-27 21:55:04 +0900

    Add documents for Camera.

--- src/camera/camera.ts
+++ src/camera/camera.ts
@@ -12,54 +12,103 @@ export default class Camera {
     this.values = [m11, m12, m21, m22, ox, oy];
   }
 
+  /**
+   * Getter property of m11
+   */
   get m11(): number {
     return this.values[0];
   }
 
+  /**
+   * Getter property of m12
+   */
   get m12(): number {
     return this.values[1];
   }
 
+  /**
+   * Getter property of m21
+   */
   get m21(): number {
     return this.values[2];
   }
 
+  /**
+   * Getter property of m22
+   */
   get m22(): number {
     return this.values[3];
   }
 
+  /**
+   * Getter property of offset x
+   */
   get ox(): number {
     return this.values[4];
   }
 
+  /**
+   * Getter property of offset y
+   */
   get oy(): number {
     return this.values[5];
   }
 
+  /**
+   * Setter property of m11
+   */
   set m11(value: number) {
     this.values[0] = value;
   }
 
+  /**
+   * Setter property of m12
+   */
   set m12(value: number) {
     this.values[1] = value;
   }
 
+  /**
+   * Setter property of m21
+   */
   set m21(value: number) {
     this.values[2] = value;
   }
 
+  /**
+   * Setter property of m22
+   */
   set m22(value: number) {
     this.values[3] = value;
   }
 
+  /**
+   * Setter property of offset x
+   */
   set ox(value: number) {
     this.values[4] = value;
   }
 
+  /**
+   * Setter property of offset y
+   */
   set oy(value: number) {
     this.values[5] = value;
   }
 
+  /**
+   * Scales the whole scene. Bigger the value is, Bigger the entities show.
+   * @param value - Scale ratio of both direction.
+   * @returns Itself which got scaled.
+   */
+  public scale(value: number): this;
+  /**
+   * Scales the whole scene. Bigger the value is, Bigger the objects show.
+   * @param x - Scale ratio of X direction.
+   * @param y - Scale ratio of Y direction.
+   * @returns Itself which got scaled.
+   */
+  public scale(x: number, y: number): this;
   public scale(x: number, y?: number): this {
     if (y === undefined) {
       return this.scale(x, x);
@@ -73,26 +122,54 @@ export default class Camera {
     return this;
   }
 
+  /**
+   * Move the camera left. (So the objects seems to move right.)
+   * @param value - Distance to move.
+   * @returns Itself which moved.
+   */
   public left(value: number): this {
     this.values[5] -= value;
     return this;
   }
 
+  /**
+   * Move the camera right. (So the objects seems to move left.)
+   * @param value - Distance to move.
+   * @returns Itself which moved.
+   */
   public right(value: number): this {
     this.values[5] += value;
     return this;
   }
 
+  /**
+   * Move the camera up. (So the objects seems to move down.)
+   * @param value - Distance to move.
+   * @returns Itself which moved.
+   */
   public up(value: number): this {
     this.values[4] -= value;
     return this;
   }
 
+  /**
+   * Move the camera down. (So the objects seems to move up.)
+   * @param value - Distance to move.
+   * @returns Itself which moved.
+   */
   public down(value: number): this {
     this.values[4] += value;
     return this;
   }
 
+  /**
+   * Apply linear transformation.
+   * @param m11 - Element of the matrix at `(1, 1)`.
+   * @param m12 - Element of the matrix at `(1, 2)`.
+   * @param m21 - Element of the matrix at `(2, 1)`.
+   * @param m22 - Element of the matrix at `(2, 2)`.
+   * @returns Itself which got applied.
+   */
   public linear(m11: number, m12: number, m21: number, m22: number): this {
     const temp = this.values.slice();
     this.values = [
@@ -106,6 +183,11 @@ export default class Camera {
     return this;
   }
 
+  /**
+   * Rotate the camera.
+   * @param angle - The rotation angle you want. (in *degrees*)
+   * @returns Itself which got rotated.
+   */
   public rotate(angle: number): this {
     angle = (angle * Math.PI) / 180;
     const sinValue = Math.sin(angle);
@@ -113,6 +195,12 @@ export default class Camera {
     return this.linear(sinValue, cosValue, -cosValue, sinValue);
   }
 
+  /**
+   * Calculate the coordinate value where a original coordinate will actually appear in the canvas.
+   * @param x - X value of the coordinate.
+   * @param y - Y value of the coordinate.
+   * @returns Actual coordinate values your coordinate will appear at.
+   */
   public calc(x: number, y: number): [number, number] {
     return [
       this.m11 * x + this.m12 * y + this.ox,
@@ -120,6 +208,11 @@ export default class Camera {
     ];
   }
 
+  /**
+   * [`DOMMatrix`]: https://developer.mozilla.org/en-US/docs/Web/API/DOMMatrix
+   * [`DOMMatrix`] getter method.
+   * @returns [`DOMMatrix`] object made from this values.
+   */
   public toDOM(): DOMMatrix {
     return new DOMMatrix([
       this.m11,
@@ -131,6 +224,11 @@ export default class Camera {
     ]);
   }
 
+  /**
+   * Apply this transformation to your canvas.
+   * @param canvas - Your canvas you want to apply this.
+   * @returns [`DOMMatrix`](https://developer.mozilla.org/en-US/docs/Web/API/DOMMatrix) object of your canvas' transformation before applying this transformation.
+   */
   public apply(canvas: CanvasRenderingContext2D): DOMMatrix {
     const beforeTransform = canvas.getTransform();
     canvas.setTransform(this.toDOM());
