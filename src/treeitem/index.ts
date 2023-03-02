690704c6b31752c80766181177b68699c6082ee7
Ailre <xtramainpp@gmail.com>
2023-03-02 11:32:30 +0900

    retype this

--- src/treeitem/index.ts
+++ src/treeitem/index.ts
@@ -1,7 +1,7 @@
 export class TreeItem {
-  protected children: (typeof this)[] = [];
+  protected children: this[] = [];
 
-  protected parent: typeof this | null = null;
+  protected parent: this | null = null;
 
   public get Parent() {
     return this.parent;
@@ -12,7 +12,7 @@ export class TreeItem {
   }
 
   public attachChildren(
-    children: (typeof this)[] | typeof this,
+    children: this[] | this,
     z_index: number = Infinity
   ): number {
     let lastIndex: number = -1;
@@ -22,7 +22,7 @@ export class TreeItem {
         lastIndex = this.attachChildren(children[index], safeIndex + index);
       }
     } else {
-      let tempParent: TreeItem | null = this;
+      let tempParent: this | null = this;
       while (tempParent !== null) {
         tempParent = this.parent;
         if (tempParent === children) {
@@ -39,7 +39,7 @@ export class TreeItem {
     return lastIndex; // if returns -1, empty array has been input.
   }
 
-  public detachChildren(children: (typeof this)[] | typeof this) {
+  public detachChildren(children: this[] | this) {
     if (Array.isArray(children)) {
       for (const child of children) {
         this.detachChildren(child);
@@ -53,11 +53,11 @@ export class TreeItem {
     }
   }
 
-  public getChildIndex(child: typeof this): number {
+  public getChildIndex(child: this): number {
     return this.children.indexOf(child);
   }
 
-  public setChildIndex(child: typeof this, index: number): number {
+  public setChildIndex(child: this, index: number): number {
     const currentIndex = this.getChildIndex(child);
     if (currentIndex === -1) {
       throw new Error("I AM NOT YOUR FATHER"); // todo: better error message?
@@ -68,7 +68,7 @@ export class TreeItem {
     return safeIndex;
   }
 
-  public setParent(parent: typeof this | null = null) {
+  public setParent(parent: this | null = null) {
     parent?.attachChildren(this);
   }
 }
