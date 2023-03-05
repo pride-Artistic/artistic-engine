b3f2347e36767cb837e137adf84b46c3fb85b0be
Ailre <xtramainpp@gmail.com>
2023-03-06 08:27:51 +0900

    [ae-151] refactor relevant test import

--- src/entity/entity.test.ts
+++ src/entity/entity.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "@jest/globals";
-import { Entity } from "./entity";
+import Entity from "./entity";
 
 describe("Entity test", () => {
   test("Parent-child", () => {
