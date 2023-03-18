b3e69d10a59530a835d9fdc169f0933d0217373c
Ailre <xtramainpp@gmail.com>
2023-03-19 02:15:20 +0900

    move granular translate methods

---
+++ src/camera/extended_camera.ts
@@ -0,0 +1,43 @@
+import Camera from "./camera";
+
+export default class ExtendedCamera extends Camera {
+  /**
+   * Move the camera left. (So the objects seems to move right.)
+   * @param value - Distance to move.
+   * @returns Itself which moved.
+   */
+  public left(value: number): this {
+    this.ox -= value;
+    return this;
+  }
+
+  /**
+   * Move the camera right. (So the objects seems to move left.)
+   * @param value - Distance to move.
+   * @returns Itself which moved.
+   */
+  public right(value: number): this {
+    this.ox += value;
+    return this;
+  }
+
+  /**
+   * Move the camera up. (So the objects seems to move down.)
+   * @param value - Distance to move.
+   * @returns Itself which moved.
+   */
+  public up(value: number): this {
+    this.oy -= value;
+    return this;
+  }
+
+  /**
+   * Move the camera down. (So the objects seems to move up.)
+   * @param value - Distance to move.
+   * @returns Itself which moved.
+   */
+  public down(value: number): this {
+    this.oy += value;
+    return this;
+  }
+}
