ed76579ee0bbb8d2d02bbfe087030dd9d6015c29
Ailre <xtramainpp@gmail.com>
2023-05-01 08:39:29 +0900

    [ae-209] include build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,5 +4,5 @@ import { Sprite } from "./sprite";
 import * as Scenes from "./scenes";
 import * as Vector from "./vector";
 import { Transform } from "./transform";
-import { Modifiers, EaseFunctions } from "./modifier";
+import { Modifiers, EaseFunctions } from "./modifiers";
 export { Engine, Entity, Sprite, Scenes, Vector, Transform, Modifiers, EaseFunctions, };
---
+++ dist/modifiers/index.d.ts
@@ -0,0 +1,3 @@
+import * as Modifiers from "./modifiers";
+import EaseFunctions from "./ease-function";
+export { Modifiers, EaseFunctions };
