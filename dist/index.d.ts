f8f72cb41698d2d98d55129213fa4b1678abdb82
Ailre <xtramainpp@gmail.com>
2023-05-05 06:59:46 +0900

    remap exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,5 +4,4 @@ import { Sprite } from "./sprite";
 import * as Scenes from "./scenes";
 import * as Vector from "./vector";
 import { Transform } from "./transform";
-import { Modifiers, EaseFunctions } from "./modifiers";
-export { Engine, Entity, Sprite, Scenes, Vector, Transform, Modifiers, EaseFunctions, };
+export { Engine, Entity, Sprite, Scenes, Vector, Transform };
