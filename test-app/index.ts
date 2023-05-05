d4f4174ffa0f5e4614911256d9daef4a8a793c74
Ailre <xtramainpp@gmail.com>
2023-05-05 14:42:20 +0900

    removed private test

--- test-app/index.ts
+++ test-app/index.ts
@@ -1,7 +1,6 @@
 import { Engine } from "../src";
-import Ailre from "./ailre";
 import GridScene from "./grid-scene";
-// import TestRectangle from "./test-rectangle";
+import TestRectangle from "./test-rectangle";
 
 document.body.style.margin = "0";
 
@@ -11,12 +10,10 @@ engine.resizeCanvas();
 const scene = new GridScene(engine.Canvas.width, engine.Canvas.height, 100);
 engine.Scene = scene;
 
-// scene.attachChildren([
-//   new TestRectangle("red", true, engine),
-//   new TestRectangle("blue", false, engine),
-// ]);
-
-scene.attachChildren(new Ailre());
+scene.attachChildren([
+  new TestRectangle("red", true, engine),
+  new TestRectangle("blue", false, engine),
+]);
 
 addEventListener("resize", () => {
   engine.resizeCanvas();
