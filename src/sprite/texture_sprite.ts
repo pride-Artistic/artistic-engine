608b786685b88a0dc1f60c40f66338a51ca1bac4
Ailre <xtramainpp@gmail.com>
2023-05-12 07:38:34 +0900

    [ae-217] define texture sprite constructor type

--- src/sprite/texture_sprite.ts
+++ src/sprite/texture_sprite.ts
@@ -1,15 +1,14 @@
 import { Sprite } from ".";
+import IEntity from "../entity/ientity";
+
+interface TextureConfig extends IEntity {
+  texture: ImageBitmap;
+}
 
 export default class TextureSprite extends Sprite {
   public texture: ImageBitmap;
 
-  constructor(config: {
-    X?: number;
-    Y?: number;
-    W?: number;
-    H?: number;
-    texture: ImageBitmap;
-  }) {
+  constructor(config: TextureConfig) {
     if (config.texture.width === 0 && config.texture.height === 0) {
       throw new Error("Texture provided is not loaded correctly");
     }
