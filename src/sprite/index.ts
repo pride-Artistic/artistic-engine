16906f3d0a7111364caca5ba7dbea1174beb67cd
Ailre <xtramainpp@gmail.com>
2023-05-12 11:31:08 +0900

    [ae-22] create text sprite w/ abstract properties

--- src/sprite/index.ts
+++ src/sprite/index.ts
@@ -2,5 +2,14 @@ import IDrawable from "./idrawable";
 import Sprite from "./sprite";
 import { Shape, Rectangle, Ellipse } from "./shapes";
 import TextureSprite from "./texture_sprite";
+import TextSprite from "./text_sprite";
 
-export { Sprite, IDrawable, Shape, Rectangle, Ellipse, TextureSprite };
+export {
+  Sprite,
+  IDrawable,
+  Shape,
+  Rectangle,
+  Ellipse,
+  TextureSprite,
+  TextSprite,
+};
