1292315770ec5b3c8dc36b4fa3fc376fe17b6886
Ailre <xtramainpp@gmail.com>
2023-05-12 11:31:40 +0900

    [ae-174] describe initial draft of font load

--- test-app/index.ts
+++ test-app/index.ts
@@ -1,4 +1,6 @@
 import { Engine } from "../src";
+import { TextSprite } from "../src/sprite";
+import { Vector2D } from "../src/vector";
 import GridScene from "./grid-scene";
 import TestRectangle from "./test-rectangle";
 
@@ -9,6 +11,24 @@ engine.resizeCanvas();
 
 const scene = new GridScene(engine.Canvas.width, engine.Canvas.height, 100);
 engine.Scene = scene;
+
+const f = new FontFace(
+  "Poppin",
+  "url(https://fonts.gstatic.com/s/poppins/v20/pxiByp8kv8JHgFVrLGT9Z1xlFQ.woff2)"
+);
+
+f.load()
+  .then(() => {
+    document.fonts.add(f);
+    const text = new TextSprite();
+    text.Property.fill = "white";
+    text.Property.font = "500 20px Poppin";
+    text.Text = "artistic baby 🥳";
+    text.Position = new Vector2D(700, 400);
+
+    scene.attachChildren(text);
+  })
+  .catch(console.log);
 
 scene.attachChildren([
   new TestRectangle("red", true, engine),
