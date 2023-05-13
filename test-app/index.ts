90e2e9f7eff6ecf9a4b9f0573a921010b532fc28
Ailre <xtramainpp@gmail.com>
2023-05-13 17:18:51 +0900

    [ae-221] describe transform plroblems w/ test app

--- test-app/index.ts
+++ test-app/index.ts
@@ -1,4 +1,4 @@
-import { Engine } from "../src";
+import { Engine, Transform } from "../src";
 import { TextSprite } from "../src/sprite";
 import { Vector2D } from "../src/vector";
 import GridScene from "./grid-scene";
@@ -12,6 +12,7 @@ engine.resizeCanvas();
 const scene = new GridScene(engine.Canvas.width, engine.Canvas.height, 100);
 engine.Scene = scene;
 
+// START FONT LOADING EXAMPLE
 const f = new FontFace(
   "Poppin",
   "url(https://fonts.gstatic.com/s/poppins/v20/pxiByp8kv8JHgFVrLGT9Z1xlFQ.woff2)"
@@ -22,13 +23,14 @@ f.load()
     document.fonts.add(f);
     const text = new TextSprite();
     text.Property.fill = "white";
-    text.Property.font = "500 20px Poppin";
-    text.Text = "artistic baby 🥳";
+    text.Property.font = "500 80px Poppin";
+    text.Text = "artistic engine 🥳";
     text.Position = new Vector2D(700, 400);
 
     scene.attachChildren(text);
   })
   .catch(console.log);
+// END FONT LOADING EXAMPLE
 
 scene.attachChildren([
   new TestRectangle("red", true, engine),
@@ -39,6 +41,23 @@ addEventListener("resize", () => {
   engine.resizeCanvas();
   scene.Width = engine.Canvas.width;
   scene.Height = engine.Canvas.height;
+});
+
+const usingTransform = true;
+
+const t = new Transform();
+t.rotate(180 / -12);
+t.translate(200, 100);
+
+// scene.Transform = t;
+// TODO: check matrix sequence
+
+engine.setSubResetFunction((context: CanvasRenderingContext2D) => {
+  if (usingTransform) context.setTransform(t.toDOM());
+  else {
+    context.translate(200, 100);
+    context.rotate(Math.PI / 12);
+  }
 });
 
 engine.start();
