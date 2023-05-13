f677417df3471d198dbcbdf530dfa515ddad77e0
Ailre <xtramainpp@gmail.com>
2023-05-13 17:18:19 +0900

    [ae-158] fix grid scene to apply transform

--- test-app/grid-scene.ts
+++ test-app/grid-scene.ts
@@ -1,4 +1,4 @@
-import { Entity, Sprite } from "../src";
+import { Sprite, Transform } from "../src";
 
 export default class GridScene extends Sprite {
   private gridSize: number;
@@ -11,37 +11,48 @@ export default class GridScene extends Sprite {
 
   private maxy = -Infinity;
 
+  private invertedTransform = new Transform();
+
   constructor(width: number, height: number, gridSize: number) {
     super({
       W: width,
       H: height,
     });
     this.gridSize = gridSize;
-    this.Region = new Entity();
-    this.Region.Dimension = this.Dimension;
+    this.Region = () => {
+      const p = new Path2D();
+      p.moveTo(this.minx, this.miny);
+      p.lineTo(this.minx, this.maxy);
+      p.lineTo(this.maxx, this.maxy);
+      p.lineTo(this.maxx, this.miny);
+      p.closePath();
+      return p;
+    };
   }
 
   public override beforeClip(context: CanvasRenderingContext2D): void {
-    const m = context.getTransform();
-    const m2 = m.invertSelf();
+    const t = Transform.fromDOM(context.getTransform());
+    const c = this.Transform.copy();
+    c.multiply(t); // TODO: check multiply sequence
+    this.invertedTransform = t.invert();
+
     const coorx = [0, context.canvas.width];
     const coory = [0, context.canvas.height];
+
     for (const x of coorx) {
       for (const y of coory) {
-        const xx = x * m2.a + y * m2.c + 1 * m2.e;
-        const yy = x * m2.b + y * m2.d + 1 * m2.f;
+        const v = this.invertedTransform.apply(x, y);
+        const xx = v.X;
+        const yy = v.Y;
         this.minx = Math.min(xx, this.minx);
         this.maxx = Math.max(xx, this.maxx);
         this.miny = Math.min(yy, this.miny);
         this.maxy = Math.max(yy, this.maxy);
       }
     }
+
     this.Width = this.maxx - this.minx;
     this.Height = this.maxy - this.miny;
-    if (this.Region) {
-      this.Region.X = this.minx;
-      this.Region.Y = this.miny;
-    }
   }
 
   public override onDraw(context: CanvasRenderingContext2D): void {
@@ -58,6 +69,7 @@ export default class GridScene extends Sprite {
       context.moveTo(i, this.miny);
       context.lineTo(i, this.maxy);
     }
+
     for (
       let i = Math.floor(this.miny / 100) * 100 + this.gridSize;
       i < this.maxy;
