fe814d0259b0174c64124b51c058b090d6964c3a
Ailre <xtramainpp@gmail.com>
2023-06-30 21:59:57 +0900

    [ae-228] review font wrapper w/ test app

--- test-app/index.ts
+++ test-app/index.ts
@@ -15,47 +15,49 @@ const scene = new GridScene(engine.Canvas.width, engine.Canvas.height, 100);
 engine.Scene = scene;
 
 // START FONT LOADING EXAMPLE
-const f = new FontFace(
-  "Poppin",
+
+const font = "Poppin";
+engine.AssetLoader.addFont(
+  font,
   "url(https://fonts.gstatic.com/s/poppins/v20/pxiByp8kv8JHgFVrLGT9Z1xlFQ.woff2)"
 );
 
-const text = new TextSprite();
-f.load()
-  .then(() => {
-    const fontBuilder = new FontBuilder("Poppin");
-    fontBuilder.setSize("80px").setWeight("500");
-    console.log(fontBuilder.toString());
-
-    document.fonts.add(f);
-    text.Property.fill = "white";
-    text.Property.font = fontBuilder.toString();
-    text.Text = "artistic engine 🥳";
-    text.Position = new Vector2D(500, 300);
-
-    scene.attachChildren(text);
-  })
-  .catch(console.log);
-
 const engineText = "artistic engine 🥳";
-let lastTick = Date.now();
-setInterval(() => {
-  engine.registerModifier(
-    new Modifier(
-      0,
-      engineText.length,
-      2000,
-      (v) => {
-        v = Math.ceil(v);
-        if (v === 17) v = 16;
-        if (Date.now() - lastTick < 20 && v != engineText.length) return;
-        lastTick = Date.now();
-        text.Text = engineText.slice(0, v);
-      },
-      EaseFunctions.EaseOutCirc
-    )
-  );
-}, 4000);
+const text = new TextSprite();
+
+engine.AssetLoader.onLoad = () => {
+  const fontBuilder = new FontBuilder(font);
+  fontBuilder.setSize("80px").setWeight("500");
+  console.log(fontBuilder.toString());
+
+  text.Property.fill = "white";
+  text.Property.font = fontBuilder.toString();
+  text.Text = engineText;
+  text.Position = new Vector2D(500, 300);
+
+  scene.attachChildren(text);
+
+  let lastTick = Date.now();
+  setInterval(() => {
+    engine.registerModifier(
+      new Modifier(
+        0,
+        engineText.length,
+        2000,
+        (v) => {
+          v = Math.ceil(v);
+          if (v === 17) v = 16;
+          if (Date.now() - lastTick < 20 && v != engineText.length) return;
+          lastTick = Date.now();
+          text.Text = engineText.slice(0, v);
+        },
+        EaseFunctions.EaseOutCirc
+      )
+    );
+  }, 4000);
+};
+
+engine.AssetLoader.load();
 
 // END FONT LOADING EXAMPLE
 
