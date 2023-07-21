6fd6b7a1f2c7653e2ad8f50ffcfcdabf8f09ded3
artistXenon <xtramainpp@gmail.com>
2023-07-21 20:05:50 +0900

    abstract structure of pointer group

--- src/event/event_groups/pointer_event_group.ts
+++ src/event/event_groups/pointer_event_group.ts
@@ -3,41 +3,65 @@ import Engine from "../../engine";
 import { Sprite } from "../../sprite";
 // import { Transform } from "../../transform";
 
-interface ITouchListener {
+interface IPointerListener {
+  TouchRegistered: boolean;
   onTouch(e: PointerEvent): boolean;
 }
 
-type TouchListener = ITouchListener & Sprite;
+type PointerListener = IPointerListener & Sprite;
 
 export default class PointerEventGroup extends EventGroup {
   protected engine: Engine;
 
-  protected iTouchListeners: TouchListener[] = [];
+  protected iTouchListeners: PointerListener[] = [];
 
-  constructor(engine: Engine, defaultTarget: EventTarget) {
+  protected childFirst: boolean = true;
+
+  constructor(engine: Engine) {
     // Maybe we should give developers a chance to select targets for each event
     // Like: [target_for_keyup, target_for_keydown, target_for_keypress]
-    super([{ event: "keyup" }, { event: "keydown" }], defaultTarget);
-    // pointerover
-    // pointerenter
-    // pointerdown
-    // pointermove
-    // pointerup
-    // pointercancel
-    // pointerout
-    // pointerleave
+    super(
+      [
+        { event: "pointerover" },
+        { event: "pointerenter" },
+        { event: "pointerdown" },
+        { event: "pointermove" },
+        { event: "pointerup" },
+        { event: "pointercancel" },
+        { event: "pointerout" },
+        { event: "pointerleave" },
+      ],
+      engine.Canvas
+    );
     // gotpointercapture
     // lostpointercapture
+    this.engine = engine;
   }
 
-  public registerTouchListener(iTouchListener: TouchListener) {
+  public registerTouchListener(iTouchListener: PointerListener) {
     // TODO: arrange listeners to meet sequence between entities.
     // top sprites will control whether to allow event to be triggered on lower sprites.
     this.iTouchListeners.push(iTouchListener);
     // this.engine.Camera.multiply(iTouchListener.Transform);
+
+    this.organizeListenerSequence();
   }
 
-  public setListener(): void {
+  public setListenSequenceChildToParent(childFirst: boolean) {
+    this.childFirst = childFirst;
+    this.organizeListenerSequence();
+  }
+
+  public override setListener(): void {
     throw new Error("Directly modifying pointer listener is forbidden");
+  }
+
+  private organizeListenerSequence() {
+    // wow
+    if (this.childFirst) {
+      // hmm
+    } else {
+      // hmmmmm
+    }
   }
 }
